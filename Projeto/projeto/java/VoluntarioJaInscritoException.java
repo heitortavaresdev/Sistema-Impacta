7899311117c5013abe5cfd902ac25e5b8f47f9fe
heitortavaresdev <heitorqueirogatavares@gmail.com>
2026-09-03 04:08:27 -0300

    Delete Projeto directory

--- Projeto/projeto/java/VoluntarioJaInscritoException.java
+++
@@ -1,5 +0,0 @@
-package Projeto.projeto.java;
-
-public class VoluntarioJaInscritoException extends RuntimeException {
-    public VoluntarioJaInscritoException(String mensagem){super(mensagem);}
-}
